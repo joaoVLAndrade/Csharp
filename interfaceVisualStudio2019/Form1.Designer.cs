a8c35454449e84763f7b643d3dfd44ed37c61114
joaoVLAndrade <42420981+joaoVLAndrade@users.noreply.github.com>
2019-05-15 13:47:43 -0300

    Add files via upload

---
+++ interfaceVisualStudio2019/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace interfaceVisualStudio2019
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.btnConectar = new System.Windows.Forms.Button();
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.textBoxEnviar = new System.Windows.Forms.TextBox();
+            this.textReceber = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(112, 11);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // btnConectar
+            // 
+            this.btnConectar.Location = new System.Drawing.Point(12, 12);
+            this.btnConectar.Name = "btnConectar";
+            this.btnConectar.Size = new System.Drawing.Size(94, 23);
+            this.btnConectar.TabIndex = 1;
+            this.btnConectar.Text = "Conectar";
+            this.btnConectar.UseVisualStyleBackColor = true;
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.Location = new System.Drawing.Point(12, 42);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(94, 23);
+            this.btnEnviar.TabIndex = 2;
+            this.btnEnviar.Text = "Enviar";
+            this.btnEnviar.UseVisualStyleBackColor = true;
+            // 
+            // textBoxEnviar
+            // 
+            this.textBoxEnviar.Location = new System.Drawing.Point(112, 43);
+            this.textBoxEnviar.Name = "textBoxEnviar";
+            this.textBoxEnviar.Size = new System.Drawing.Size(676, 22);
+            this.textBoxEnviar.TabIndex = 3;
+            // 
+            // textReceber
+            // 
+            this.textReceber.Location = new System.Drawing.Point(12, 71);
+            this.textReceber.Multiline = true;
+            this.textReceber.Name = "textReceber";
+            this.textReceber.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textReceber.Size = new System.Drawing.Size(776, 386);
+            this.textReceber.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textReceber);
+            this.Controls.Add(this.textBoxEnviar);
+            this.Controls.Add(this.btnEnviar);
+            this.Controls.Add(this.btnConectar);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button btnConectar;
+        private System.Windows.Forms.Button btnEnviar;
+        private System.Windows.Forms.TextBox textBoxEnviar;
+        private System.Windows.Forms.TextBox textReceber;
+    }
+}
+
---
+++ interfaceVisualStudio2019/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Ports;
+
+namespace interfaceVisualStudio2019
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+        private void AtualizListaCOMs()
+        {
+            int i;
+            bool quantDiferente;
+            i = 0;
+            quantDiferente = false;
+            if (comboBox1.Items.Count == SerialPort.GetPortNames().Length)
+            {
+                foreach (string s in SerialPort.GetPortNames())
+                {
+                    if (comboBox1.Items[i++].Equals(s) == false)
+                    {
+                        quantDiferente = true;
+                    }
+                }
+            }
+            else
+            {
+                quantDiferente = true;
+            }
+            if(quantDiferente == false)
+            {
+                return;
+            }
+            comboBox1.Items.Clear();
+            foreach(string s in SerialPort.GetPortNames())
+            {
+                comboBox1.Items.Add(s);
+            }
+            comboBox1.SelectedIndex = 0;
+        }
+    }
+}
