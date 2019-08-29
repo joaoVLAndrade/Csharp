5f8a9eee90f2e62640032055a91f8688122c9d73
joaoVLAndrade <42420981+joaoVLAndrade@users.noreply.github.com>
2019-08-28 21:17:34 -0300

    Add files via upload
    Operations with vetors in R2

---
+++ caLAB1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace caLAB1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vetor2D V1 = new Vetor2D();
+            Vetor2D V2 = new Vetor2D();
+            Console.WriteLine("Digite a coordenada x do v1: ");
+            V1.setX(Convert.ToDouble(Console.ReadLine()));
+            Console.WriteLine("Digite a coordenada y do v1: ");
+            V1.setY(Convert.ToDouble(Console.ReadLine()));
+            Console.WriteLine("Digite a coordenada x do v2: ");
+            V2.setX(Convert.ToDouble(Console.ReadLine()));
+            Console.WriteLine("Digite a coordenada y do v2: ");
+            V2.setY(Convert.ToDouble(Console.ReadLine()));
+            Console.WriteLine("Vetor 1: (" + V1.getX() + "," + V1.getY() + ")");
+            Console.WriteLine("Vetor 2: (" + V2.getX() + "," + V2.getY() + ")");
+            V1.produtoInterno(V1, V2);
+            V1.modulo(V1, V2);
+            V1.angulo();
+            V1.projecao(V1,V2);
+        }
+    }
+}
---
+++ caLAB1/Vetor2D.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace caLAB1
+{
+    class Vetor2D
+    {
+        //Atributos
+        private double mod1, mod2, escalar, ang;
+        private double x;
+        private double y;
+        //Construtor
+        public Vetor2D()
+        {
+            x = 0.0;
+            y = 0.0;
+        }
+        public void setX(double novoX)
+        {
+            x = novoX;
+        }
+        public void setY(double novoY)
+        {
+            y = novoY;
+        }
+        public double getX()
+        {
+            return x;
+        }
+        public double getY()
+        {
+            return y;
+        }
+        public void produtoInterno(Vetor2D v1,Vetor2D v2)
+        {
+            //Produto interno entre vetores --> <u,v> = x1*x2+y1*y2
+            escalar = v1.getX() * v2.getX() + v1.getY() * v2.getY();
+            Console.WriteLine("O produto escalar entre V1 e V2 é: " + escalar);
+        }
+        public void modulo(Vetor2D v1,Vetor2D v2)
+        {
+            //Módulo de um vetor = (x² + y²)^(1/2)
+            mod1 = Math.Sqrt((Math.Pow(v1.getX(), 2) + Math.Pow(v1.getY(), 2)));
+            mod2 = Math.Sqrt((Math.Pow(v2.getX(), 2) + Math.Pow(v2.getY(), 2)));
+            Console.WriteLine("O módulo do V1 é: " + mod1 + " e o módulo do V2 é: " + mod2);
+        }
+        public void angulo()
+        {
+            //Angulo entre os dois vetores ---> (<u.v>/||u||*||v||)
+            ang = (Math.Acos(escalar/(mod1*mod2)));
+            Console.WriteLine("O angulo entre os vetores é: " + ang * 180 / Math.PI + " graus");
+        }
+        public void projecao(Vetor2D v1, Vetor2D v2)
+        {       
+            //projeção de v1 em v2
+            double X1 = (escalar / (mod2*mod2)) * v2.getX();
+            double Y1 = (escalar / (mod2*mod2)) * v2.getY();
+            //projeção de v2 em v1
+            double X2 = (escalar / (mod1 * mod1)) * v1.getX();
+            double Y2 = (escalar / (mod1 * mod1)) * v1.getY();
+            Console.WriteLine("O vetor resultante da projeção de V1 em V2 é: (" + X1 + "," + Y1 + ")");
+            Console.WriteLine("O vetor resultante da projeção de V2 em V1 é: (" + X2 + "," + Y2 + ")");
+        }
+    }
+}
